d55554636c293337d48df2bd67f1646ba52e433f
estelle <est.picq@gmail.com>
2018-11-24 19:47:05 +0100

    handle absolute and static position

--- src/app/draggable.directive.ts
+++ src/app/draggable.directive.ts
@@ -1,12 +1,11 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
 import { map, takeUntil, mergeMap, filter, tap } from 'rxjs/operators';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 
 interface MousePosition {
   top: number;
   left: number;
-  maxTop: number;
-  maxLeft: number;
 }
 
 @Directive({
@@ -18,6 +17,7 @@ export class DraggableDirective implements OnInit {
   @Input() bounds: HTMLElement;
   draggable: HTMLElement;
   handle: HTMLElement;
+  draggablePosition: string;
   mousedown$: Observable<MouseEvent>;
   mousemove$: Observable<MouseEvent>;
   mouseup$: Observable<MouseEvent>;
@@ -28,9 +28,12 @@ export class DraggableDirective implements OnInit {
   ) { }
 
   ngOnInit() {
+    // Find draggable element
     this.draggable = this.el.nativeElement;
-    this.handle = this.draggable;
+    this.draggablePosition = window.getComputedStyle(this.draggable).getPropertyValue('position');
 
+    // Handle from a specific part of draggable element
+    this.handle = this.draggable;
     if (this.draggableFrom) {
       this.handle = this.draggable.querySelector('#' + this.draggableFrom);
     }
@@ -45,23 +48,14 @@ export class DraggableDirective implements OnInit {
       tap(() => document.body.classList.add('no-select')),
       mergeMap(mdEvent => {
         // get pointer start position
-        const startX = mdEvent.clientX - parseInt(this.draggable.style.left, 10);
-        const startY = mdEvent.pageY - parseInt(this.draggable.style.top, 10);
-        // get container bounds
-        let maxLeft = Infinity;
-        let maxTop = Infinity;
-        if (this.bounds) {
-          maxLeft = this.bounds.getBoundingClientRect().width;
-          maxTop = this.bounds.getBoundingClientRect().height;
-        }
+        const startX = mdEvent.clientX - +this.draggable.getAttribute('data-x');
+        const startY = mdEvent.pageY - +this.draggable.getAttribute('data-y');
         // listen to mousemove
         return this.mousemove$.pipe(
           map(mmEvent => {
             return {
               left: mmEvent.clientX - startX,
               top: mmEvent.pageY - startY,
-              maxLeft: maxLeft,
-              maxTop: maxTop
             };
           }),
           // stop listening to mousemove on mouseup
@@ -76,10 +70,42 @@ export class DraggableDirective implements OnInit {
     );
 
     this.mousedrag$.subscribe(pos => {
-      if (pos.top >= 0 && pos.left >= 0 && pos.top <= pos.maxTop && pos.left <= pos.maxLeft) {
-        this.draggable.style.top = pos.top + 'px';
-        this.draggable.style.left = pos.left + 'px';
+      // get container bounds
+      let containerWidth = Infinity;
+      let containerHeight = Infinity;
+      let containerTop = 0;
+      let containerLeft = 0;
+      if (this.bounds) {
+        containerWidth = this.bounds.getBoundingClientRect().width;
+        containerHeight = this.bounds.getBoundingClientRect().height;
+        containerLeft = this.bounds.getBoundingClientRect().left;
+        containerTop = this.bounds.getBoundingClientRect().top;
       }
+      // get draggable bounds
+      const draggableWidth = this.draggable.getBoundingClientRect().width;
+      // Logic here
+      let finalLeft: number = pos.left;
+      let finalTop: number = pos.top;
+        if (this.draggablePosition === 'absolute'
+            || this.draggablePosition === 'fixed'
+            || this.draggablePosition === 'sticky'
+            || this.draggablePosition === 'relative') { // top and left properties can apply
+                this.bounds.style.position = 'relative';
+                if (pos.left < 0) { finalLeft = 0; }
+                if (pos.left > containerWidth - draggableWidth) { finalLeft = containerWidth - draggableWidth; }
+                if (pos.top < 0) { finalTop = 0; }
+                if (pos.top > containerHeight) { finalTop = containerHeight; }
+                this.draggable.style.left = finalLeft + 'px';
+                this.draggable.style.top = finalTop + 'px';
+        } else { // top and left cannot apply - use transform instead
+          if (pos.left <  containerLeft - this.draggable.offsetLeft) { finalLeft = containerLeft - this.draggable.offsetLeft; }
+          if (pos.left >  containerWidth - draggableWidth) { finalLeft = containerWidth - draggableWidth; }
+          if (pos.top <  containerTop - this.draggable.offsetTop) { finalTop = containerTop - this.draggable.offsetTop; }
+          this.draggable.style.transform = 'translate(' + finalLeft + 'px, ' + finalTop + 'px)';
+        }
+        // In each case, set data-x and data-y
+        this.draggable.setAttribute('data-x', finalLeft.toString());
+        this.draggable.setAttribute('data-y', finalTop.toString());
     });
   }
 
@@ -88,7 +114,6 @@ export class DraggableDirective implements OnInit {
       window.getSelection().removeAllRanges();
     }
   }
-
 
 }
 
